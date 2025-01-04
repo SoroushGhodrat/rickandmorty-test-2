e9ffa09caca6ce2b628d49b137d5b23d256e5117
Soroush <s.ghodrat@gmail.com>
2025-01-04 22:44:08 +0100

    Add Residents context and provider for managing resident details

---
+++ src/context/AppProviders.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import { ResidentsProvider } from './ResidentsContext';
+
+
+interface AppProvidersProps {
+  children: ReactNode;
+}
+
+const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
+  return (
+    <ResidentsProvider>
+      {children}
+    </ResidentsProvider>
+  );
+};
+
+export default AppProviders;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import Layout from "@/components/common/layout/Layout";
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Layout from '@/components/common/layout/Layout';
+import AppProviders from '@/context/AppProviders';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppProviders>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppProviders>
   );
 }
