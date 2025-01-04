08ce8a97029a90c0c9fafb510f9117d0580184a3
Soroush <s.ghodrat@gmail.com>
2025-01-04 21:03:07 +0100

    Add custom hook for fetching multiple Axios requests

---
+++ src/hooks/useMultipleAxios.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+interface UseMultipleAxiosResult<T> {
+  data: T[];
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const useMultipleAxios = <T,>(urls: string[]): UseMultipleAxiosResult<T> => {
+  const [data, setData] = useState<T[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+
+      try {
+        const responses: AxiosResponse<T>[] = await Promise.all(
+          urls.map((url) => axios.get<T>(url))
+        );
+        const responseData = responses.map((response) => response.data);
+        setData(responseData);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (urls.length > 0) {
+      fetchData();
+    }
+  }, [urls]);
+
+  return { data, isLoading, isError };
+};
+
+export default useMultipleAxios;
