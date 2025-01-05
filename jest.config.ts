1e88c942f675c77a663c4b38dda4229e51ea3034
Soroush <s.ghodrat@gmail.com>
2025-01-05 20:33:41 +0100

    Configute Jest testing library

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageReporters: ['text', 'lcov'],
+};
+
+export default config;
