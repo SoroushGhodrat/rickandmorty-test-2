6b8d870a5fff22fde7280f503dbe6dd799061cb2
Soroush <s.ghodrat@gmail.com>
2025-01-05 16:41:47 +0100

    Fix loading component styling and correct typo in loading text

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Loading: React.FC = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-blue-900" />
-      <p className="pt-5">Loaing</p>
+      <p className="pt-5">Loading</p>
     </div>
   );
 };
