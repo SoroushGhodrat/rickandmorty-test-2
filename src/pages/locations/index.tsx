63a18edbc4205d95e9a6b292c4faa48560fb5e5a
Soroush <s.ghodrat@gmail.com>
2025-01-05 16:13:56 +0100

    Refactor Locations component to use LocationLists for rendering locations

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -1,18 +1,13 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import useAxios from '@/hooks/useAxios';
-import dateNormalizer from '@/utils/helpers/dateNormalizer';
 import Loading from '@/components/ui/Loading';
 import Error from '@/components/ui/Error';
 import Pagination from '@/components/common/Pagination';
-import { FcTodoList } from 'react-icons/fc';
-import ResidentsList from '@/components/Residents/ResidentsList';
-import { useResidents } from '@/context/ResidentsContext';
 import { ApiLocationResponse } from '@/types/types';
-import { Location } from '@/types/types';
+import LocationLists from '@/components/Locations/LocationLists';
 
 const Locations: React.FC = () => {
   const [page, setPage] = useState(1);
-  const { residentsDetailes, setResidentsDetailes } = useResidents();
 
   const axiosRequestConfig = useMemo(
     () => ({
@@ -29,17 +24,6 @@ const Locations: React.FC = () => {
     setPage(newPage);
   }, []);
 
-  // Update ResidentsList state in context
-  const handleResidentsListContext = useCallback(
-    (residentsList: string[]) => {
-      setResidentsDetailes({
-        show: !residentsDetailes.show,
-        list: residentsList,
-      });
-    },
-    [residentsDetailes, setResidentsDetailes],
-  );
-
   if (isLoading) return <Loading />;
   if (isError) return <Error />;
 
@@ -53,45 +37,8 @@ const Locations: React.FC = () => {
       </header>
 
       {isSuccessful && data && (
-        <section>
-          <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {data.results.map((location: Location) => (
-              <li
-                key={location.id}
-                className="transform rounded-md border p-4 capitalize transition-all duration-300 hover:scale-105 hover:border-blue-500 hover:shadow-xl"
-              >
-                <div className="py-8 text-center text-xl font-bold">
-                  <h2>{`Location ${location.id}`}</h2>
-                  <h2 className="text-xl font-bold">{location.name}</h2>
-                </div>
-                <p className="my-1">Type: {location.type}</p>
-                <p className="my-1">Dimension: {location.dimension}</p>
-                <p className="my-1">
-                  Number of residents: {location.residents.length}
-                </p>
-                {location.residents.length > 0 && (
-                  <div className="my-1 flex">
-                    <button
-                      onClick={() =>
-                        handleResidentsListContext(location.residents)
-                      }
-                      className="flex items-center text-blue-500 hover:text-blue-700"
-                      aria-label={`View residents of ${location.name}`}
-                    >
-                      <FcTodoList fontSize={20} />
-                      <span className="ml-2">View Residents</span>
-                    </button>
-                  </div>
-                )}
-
-                <p className="my-1">
-                  Created: {dateNormalizer(location.created)}
-                </p>
-              </li>
-            ))}
-          </ul>
-
-          {residentsDetailes.show && <ResidentsList />}
+        <>
+          <LocationLists data={data} />
 
           <Pagination
             currentPage={page}
@@ -100,7 +47,7 @@ const Locations: React.FC = () => {
             hasPrevPage={!!data.info.prev}
             onPageChange={handlePageChange}
           />
-        </section>
+        </>
       )}
     </main>
   );
