1e4039f266fbe83544fb01a8dd499a862c83a9eb
Soroush <s.ghodrat@gmail.com>
2025-01-05 16:57:51 +0100

    Update - Replace the images.domains configuration with images.remotePatterns based on the latest Next.js documentation.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['rickandmortyapi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rickandmortyapi.com',
+      },
+    ],
   },
 };
 
