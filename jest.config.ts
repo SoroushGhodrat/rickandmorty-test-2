1d20e7413d81577edbb0b8397fbc18c87301a9a7
Soroush <s.ghodrat@gmail.com>
2025-01-06 09:27:55 +0100

    Use forceSwcTransforms to temporarily fix the Babel conflict.

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,21 @@ import type { Config } from 'jest';
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  coverageReporters: ['text', 'lcov'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 };
 
 export default config;
