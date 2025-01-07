1dd3d16572f63eda18d4f858368613b4d399e1a9
Soroush <s.ghodrat@gmail.com>
2025-01-07 14:50:57 +0100

    Add API endpoint constants for episodes, locations, and characters

---
+++ src/constants/urls.ts
@@ -0,0 +1,5 @@
+const EPISODES_URL = 'https://rickandmortyapi.com/api/episode';
+const LOCATION_URL = 'https://rickandmortyapi.com/api/location';
+const CHARACTER_URL = 'https://rickandmortyapi.com/api/character';
+
+export { EPISODES_URL, LOCATION_URL, CHARACTER_URL };
